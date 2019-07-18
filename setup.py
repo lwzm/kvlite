bcce905838fdc1446d7959bc0dde88583526e1e2
lwzm <lwzm@qq.com>
2019-07-18 10:19:53 +0800

    update api

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README") as f:
 
 setup(
     name=name,
-    version="0.5",
+    version="0.6",
     description="kvlite",
     long_description=long_description,
     long_description_content_type="text/markdown",
